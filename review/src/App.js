edc04ebf2704a197159a839d002c3dfa59b8b7a9
Warren Longmire <warren.longmire@lambdaschool.com>
2021-07-07 12:32:32 -0400

    lecture work-in-progress

--- review/src/App.js
+++ review/src/App.js
@@ -3,6 +3,8 @@ import "./styles.scss";
 import axios from 'axios';
 import data from "../data";
 
+import SelectedPokemon from './components/SelectedPokemon';
+
 function App() {
   const [pokemen, setPokemen] = useState([]);
   const [selectedPokemon, setSelectedPokemon] = useState({});
@@ -20,27 +22,7 @@ function App() {
 
   return (
     <div className="App">
-      <div id="selectedDiv">
-        <h2>Selected Pokemon: {selectedPokemon.name || "none"}</h2>
-        {selectedPokemon.name && (
-          <div>
-            <img
-              src={selectedPokemon.sprites.front_default}
-              alt={selectedPokemon.sprites.front_default}
-            />
-            <h5>Height: {selectedPokemon.height}</h5>
-            <h5>Weight: {selectedPokemon.weight}</h5>
-            <h5>Abilities:</h5>
-            <ul>
-              {selectedPokemon.abilities.map((a) => (
-                <li key={a.ability.name}>
-                  {a.ability.name} - Slot {a.slot}{" "}
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
-      </div>
+      <SelectedPokemon selectedPokemon={selectedPokemon} />
       <div id="pokeList">
         {pokemen.map((pokemon) => (
           <div
