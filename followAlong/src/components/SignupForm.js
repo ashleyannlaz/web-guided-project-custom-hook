02c5c650ad122686b2a523616a5d93aef48a64d2
ashleyannlaz <paperpastels@gmail.com>
2021-08-04 13:28:00 -0400

    Set up

--- followAlong/src/components/SignupForm.js
+++ followAlong/src/components/SignupForm.js
@@ -3,40 +3,32 @@ import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 
 import Button from "../theme/Button";
+// 1. create useForm
+// 2. add in stateful logic into useForm
+// 3. return all needed items from useForm
+// 4. connect useForm to our SignupForm
 
-const useStyles = makeStyles(theme => ({
-  container: {
-    display: "flex",
-    flexWrap: "wrap"
-  },
-  textField: {
-    marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1)
-  },
-  dense: {
-    marginTop: theme.spacing(2)
-  },
-  menu: {
-    width: 200
-  }
-}));
-
-export default function SignupForm() {
-  const classes = useStyles();
-  const [firstName, setFirstName] = useState("");
+const useForm = (initialValue) => {
+  const [firstName, setFirstName] = useState(initialValue);
 
   const handleChanges = e => {
     setFirstName(e.target.value);
   };
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    alert(firstName);
-  };
-
   const clearForm = e => {
     e.preventDefault();
     setFirstName("");
+  };
+  return [firstName, handleChanges, clearForm];
+}
+
+export default function SignupForm() {
+  const classes = useStyles();
+  const [firstName, handleChanges, clearForm] = useForm("Ashley");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    alert(firstName);
   };
 
   return (
@@ -67,3 +59,20 @@ export default function SignupForm() {
     </div>
   );
 }
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1)
+  },
+  dense: {
+    marginTop: theme.spacing(2)
+  },
+  menu: {
+    width: 200
+  }
+}));
